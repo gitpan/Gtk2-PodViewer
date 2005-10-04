b06bcbc22d135f5218021d52145320cc7348e0d7
Gavin Brown <gbrown@cpan.org>
2005-10-04 12:14:19 +0000

    Import of GBROWN/Gtk2-PodViewer-0.13 from CPAN.
    gitpan-cpan-distribution: Gtk2-PodViewer
    gitpan-cpan-version:      0.13
    gitpan-cpan-path:         GBROWN/Gtk2-PodViewer-0.13.tar.gz
    gitpan-cpan-author:       GBROWN
    gitpan-cpan-maturity:     released

---
+++ PodViewer.pm
@@ -0,0 +1,34 @@
+package Gtk2::PodViewer;
+use Carp;
+use strict;
+
+our $VERSION = '0.13';
+
+warn('Gtk2::PodViewer is deprecated; use Gtk2::Ex::PodViewer instead');
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Gtk2::PodViewer - a stub module for Gtk2::Ex::PodViewer
+
+=head1 SYNOPSIS
+
+Gtk2::PodViewer was renamed to Gtk2::Ex::PodViewer on 2005-10-04. This module,
+such that it is, exists merely as a stub to allow CPAN.pm to install the
+correct module.
+
+=head1 SEE ALSO
+
+L<Gtk2::Ex::PodViewer>
+
+=head1 AUTHOR
+
+Gavin Brown (gavin dot brown at
+uk dot com)
+
+=cut
